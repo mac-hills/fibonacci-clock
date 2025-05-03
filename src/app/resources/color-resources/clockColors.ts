8babb6429a7a04ca6fc770ab4370564322e41afa
mac-hills <159528363+mac-hills@users.noreply.github.com>
2025-05-03 15:50:01 +0200

    Update clockColors.ts

--- src/app/resources/color-resources/clockColors.ts
+++ src/app/resources/color-resources/clockColors.ts
@@ -1,16 +1,10 @@
 export const clockColors: { [key: string]: string } = {
-  colorWhenUsedForHoursAndMinutes:      'rgb(150, 120, 182)', // lavender purple
-  colorWhenUsedForHours :               'rgb(255,102,102)',   // coral pink
-  colorWhenUsedForMinutes:              'rgb(0,128,128)',     // Teal blue
-  colorWhenNotUsed:                     'rgb(255, 204, 0)',   // golden yellow
-  clockBackGroundColorArrayStartColor:  'rgb(255,102,102)',   // coral pink
-  clockBackGroundColorArrayEndColor:    'rgb(0, 128, 64)',    // Emerald Green
-  secondsCounterArrayStartColor:        'rgb(0, 128, 64)',    // Emerald Green
-  secondsCounterArrayEndColor:          'rgb(228, 0, 171)'     // pink
+  colorWhenUsedForHoursAndMinutes:      'rgb(100, 139, 51)',
+  colorWhenUsedForHours :               'rgb(67, 172, 97)',
+  colorWhenUsedForMinutes:              'rgb(50, 116, 49)',
+  colorWhenNotUsed:                     'rgb(101, 199, 97)',
+  clockBackGroundColorArrayStartColor:  'rgb(50, 116, 49)',
+  clockBackGroundColorArrayEndColor:    'rgb(64, 161, 87)',
+  secondsCounterArrayStartColor:        'rgb(101, 199, 97)',
+  secondsCounterArrayEndColor:          'rgb(64, 161, 87)'
 }
-// Theme colors SAM
-    // orange   'rgb(243, 146, 0)'  
-    // purple   'rgb(158, 25, 129)' 
-    // green    'rgb(118, 184, 42)' 
-    // pinkred: 'rgb(228, 0, 58)'   
-    // blue:    'rgb(0, 179, 217)'  
